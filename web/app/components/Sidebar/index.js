e272ff651404b3a238792384ee7e14d3928eef6d
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2017-04-25 19:57:04 +0200

    Experiment with webpack SASS loader.

--- web/app/components/Sidebar/index.js
+++ web/app/components/Sidebar/index.js
@@ -4,32 +4,33 @@ import styled from 'styled-components';
 
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
+import styles from './styles.scss';
 
-const SidebarContainer = styled.div`
-  background-color: #292929;
-  color: #FFFFFF;
-  padding: 10px;
-`;
+// const SidebarContainer = styled.div`
+//   background-color: #292929;
+//   color: #FFFFFF;
+//   padding: 10px;
+// `;
 
-const LinkList = styled.ul`
-  list-style: none;
-  padding-left: 0;
-
-  li {
-    padding-bottom: 100px;
-  }
-
-  li a {
-    color: #FFFFFF;
-    text-decoration: none;
-  }
-`;
+// const LinkList = styled.ul`
+//   list-style: none;
+//   padding-left: 0;
+//
+//   li {
+//     padding-bottom: 100px;
+//   }
+//
+//   li a {
+//     color: #FFFFFF;
+//     text-decoration: none;
+//   }
+// `;
 
 class Sidebar extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <SidebarContainer>
-        <LinkList>
+      <div className="sidebar">
+        <ul className="sidelinks">
           <li>
             <Link to="/">
               <FormattedMessage {...messages.home} />
@@ -45,8 +46,8 @@ class Sidebar extends React.PureComponent { // eslint-disable-line react/prefer-
               <FormattedMessage {...messages.praise} />
             </Link>
           </li>
-        </LinkList>
-      </SidebarContainer>
+        </ul>
+      </div>
     );
   }
 }
