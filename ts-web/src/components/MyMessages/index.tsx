c52b0d348e145a67198f7cbfdfb3a5a5fb2743b5
Rocky Jaiswal <rockyj@rockyj-home.fritz.box>
2017-07-23 19:01:23 +0200

    Flex based formatting for responsive design.

--- ts-web/src/components/MyMessages/index.tsx
+++ ts-web/src/components/MyMessages/index.tsx
@@ -13,24 +13,20 @@ class MyMessages extends React.PureComponent<IProps> {
   render() {
     return (
       <div className={styles.messages}>
-        <table>
-          <tbody>
-            {this.props.messages.map((message) => {
-              return (
-                <tr key={message.id}>
-                  <td>{message.messageText.substr(0, 20)}...</td>
-                  <td>Edit</td>
-                  <td>
-                    <button className={styles.deleteButton}
-                      onClick={() => this.props.deleteMessage(message.id)}>
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        {this.props.messages.map((message) => {
+          return (
+            <div className={styles.message} key={message.id}>
+              <span>{message.messageText.substr(0, 20)}...</span>
+              <span>Edit</span>
+              <span>
+                <button className={styles.deleteButton}
+                  onClick={() => this.props.deleteMessage(message.id)}>
+                  Delete
+                </button>
+              </span>
+            </div>
+          );
+        })}
       </div>
     );
   }
