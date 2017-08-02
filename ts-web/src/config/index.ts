52895a1a781cc6507ed2bfdafaa12f48e0ada2ac
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2017-08-02 16:44:20 +0200

    prepare for prod.

--- ts-web/src/config/index.ts
+++ ts-web/src/config/index.ts
@@ -10,10 +10,13 @@ const environmentConfiguration = (environment: string) => {
       baseURL: 'http://' + window.location.hostname + ':3001'
     }).toObject();
   }
+  return defaultConf.merge({
+    baseURL: 'http://prayersnpraises.online/api'
+  }).toObject();
 };
 
 const Config = {
-  env: environmentConfiguration('development')
+  env: environmentConfiguration(process.env.APP_ENV || 'development')
 };
 
 export default Config;
--- ts-web/webpack.config.js
+++ ts-web/webpack.config.js
@@ -87,6 +87,7 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        APP_ENV: JSON.stringify(process.env.APP_ENV),
         AUTH0_DOMAIN: JSON.stringify(process.env.AUTH0_DOMAIN),
         AUTH0_CLIENT_ID: JSON.stringify(process.env.AUTH0_CLIENT_ID),
         AUTH0_AUDIENCE: JSON.stringify(process.env.AUTH0_AUDIENCE)
